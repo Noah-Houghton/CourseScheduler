4e856f0aa18d35cfcae4229c345c3075f5be4bf4
Noah Houghton <noah.houghton.97@gmail.com>
2018-01-01 14:55:00 -0800

    initial commit

---
+++ fresh/application.py
@@ -0,0 +1,4 @@
+# A web application to rapidly prototype courses based on time conflicts
+# functionality extension: track requirements and progress towards completion
+
+# requirements: front-end to add courses and manage existing courses
